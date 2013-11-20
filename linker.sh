01f9a9f8a8f8aa0b2caefe62a12f609eb5f4b11c
Vaarnan Drolia <vaarnan@gmail.com>
2013-11-20 16:40:34 +0800

    Added gitconfig and gitignore_global

--- linker.sh
+++ linker.sh
@@ -3,5 +3,7 @@ ln -s $PWD/.vim $HOME/.vim
 ln -s $PWD/.vimrc $HOME/.vimrc
 ln -s $PWD/.bash_aliases $HOME/.bash_aliases
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s $PWD/git/gitignore_global $HOME/.gitignore_global
+ln -s $PWD/git/gitconfig $HOME/.gitconfig
 echo 'source $HOME/.bash_aliases'>>$HOME/.bashrc
 . $HOME/.bashrc
