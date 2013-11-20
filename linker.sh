5fd40f56293544894d087970633779ef2f94f6c1
Vaarnan Drolia <vaarnan@gmail.com>
2013-11-21 01:49:23 +0800

    Renaming files by removing period

--- linker.sh
+++ linker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ln -s $PWD/.vim $HOME/.vim
-ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.bash_aliases $HOME/.bash_aliases
-ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s $PWD/vim $HOME/.vim
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/bash_aliases $HOME/.bash_aliases
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/git/gitignore_global $HOME/.gitignore_global
 ln -s $PWD/git/gitconfig $HOME/.gitconfig
 echo 'source $HOME/.bash_aliases'>>$HOME/.bashrc
