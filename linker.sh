5ef6b5adca1b474c2771977072ee5f1f2c64ac93
Vaarnan Drolia <vaarnan@gmail.com>
2014-03-20 18:50:43 +0800

    Shifting to zsh

--- linker.sh
+++ linker.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/zshrc $HOME/.zshrc
 ln -s $PWD/aliases $HOME/.aliases
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/git/gitignore_global $HOME/.gitignore_global
 ln -s $PWD/git/gitconfig $HOME/.gitconfig
-echo 'source $HOME/.bash_aliases'>>$HOME/.bashrc
-. $HOME/.bashrc
+echo 'source $HOME/.aliases'>>$HOME/.zshrc
+. $HOME/.zshrc
