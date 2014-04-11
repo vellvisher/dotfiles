250740543c7d91ea55cecd1f3bd163ca1511c3b9
Vaarnan Drolia <vaarnan@gmail.com>
2014-04-11 23:17:55 +0800

    Tmux sync colorscheme with vim, shift to new file colorscheme.vim, new .tmux folder

--- linker.sh
+++ linker.sh
@@ -4,6 +4,7 @@ ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/zshrc $HOME/.zshrc
 ln -s $PWD/aliases $HOME/.aliases
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/tmux $HOME/.tmux
 ln -s $PWD/git/gitignore_global $HOME/.gitignore_global
 ln -s $PWD/git/gitconfig $HOME/.gitconfig
 . $HOME/.zshrc
