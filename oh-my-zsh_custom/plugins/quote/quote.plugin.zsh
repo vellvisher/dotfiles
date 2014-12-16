466d3be86a725748d5f7765b814e65f2ac415321
Vaarnan Drolia <vaarnan@gmail.com>
2014-12-16 22:13:55 +0100

    Give only no quote file log

--- oh-my-zsh_custom/plugins/quote/quote.plugin.zsh
+++ oh-my-zsh_custom/plugins/quote/quote.plugin.zsh
@@ -4,19 +4,19 @@ TEXT_COLOR="\e[0;97m"
 COLON_COLOR="\e[0;35m"
 END_COLOR="\e[m"
 
+function quote()
 if [[ -a $QUOTE_FILE ]]; then
-    function quote()
-    {
-        Q=$(gsort -R $QUOTE_FILE | head -1)
-        W=$(echo "$Q" | sed -e 's/\(.*\):\(.*\)/\1/')
-        TXT=$(echo "$Q" | sed -e 's/\(.*\):\(.*\)/\2/')
-        if [ "$W" -a "$TXT" ]; then
-          echo "${WHO_COLOR}${W}${COLON_COLOR}: ${TEXT_COLOR}“${TXT}”${END_COLOR}\n"
-        else
-          quote
-        fi
-    }
-    #quote
+{
+    Q=$(gsort -R $QUOTE_FILE | head -1)
+    W=$(echo "$Q" | sed -e 's/\(.*\):\(.*\)/\1/')
+    TXT=$(echo "$Q" | sed -e 's/\(.*\):\(.*\)/\2/')
+    if [ "$W" -a "$TXT" ]; then
+        echo "${WHO_COLOR}${W}${COLON_COLOR}: ${TEXT_COLOR}“${TXT}”${END_COLOR}\n"
+    else
+        quote
+    fi
+}
 else
     echo "quote file does not exist" >&2
 fi
+#quote
