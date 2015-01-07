c8723ddf9fd73671d9e9565f8a2cdd85fd69fa88
Vaarnan Drolia <vaarnan@gmail.com>
2015-01-07 12:07:39 +0100

    Get all git submodules

--- linker.sh
+++ linker.sh
@@ -12,3 +12,5 @@ ln -s $PWD/todo.actions.d $HOME/.todo.actions.d
 ln -s $PWD/git/gitignore_global $HOME/.gitignore_global
 ln -s $PWD/git/gitconfig $HOME/.gitconfig
 . $HOME/.zshrc
+git submodule init
+git submodule update
