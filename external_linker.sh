971cfb8da5ae204fb7518a6a9a17d274a5dcb88c
Vaarnan Drolia <vaarnan@gmail.com>
2016-08-14 08:15:54 +0100

    Add message for tpm

--- external_linker.sh
+++ external_linker.sh
@@ -1,5 +1,6 @@
 # Tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Press <TMUX-LEADER>+I to install tpm plugins"
 
 # oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
