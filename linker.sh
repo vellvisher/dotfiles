0e179c600e37a3d3c518f92f4646b93c19dbf894
Vaarnan Drolia <vaarnan@gmail.com>
2016-08-15 09:21:48 +0100

    Fix tmux colorscheme import, set colorscheme to iceberg from tmuxline

--- linker.sh
+++ linker.sh
@@ -7,7 +7,7 @@ ln -s $PWD/zshenv $HOME/.zshenv
 ln -s $PWD/aliases $HOME/.aliases
 ln -s $PWD/aliases_disable $HOME/.aliases_disable
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/tmux $HOME/.tmux
+ln -s $PWD/tmux/colorscheme.conf $HOME/.tmux/colorscheme.conf
 ln -s $PWD/tmux.reset.keys $HOME/.tmux.reset.keys
 ln -s $PWD/todo.actions.d $HOME/.todo.actions.d
 ln -s $PWD/git/gitignore_global $HOME/.gitignore_global
