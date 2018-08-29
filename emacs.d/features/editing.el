76f0602a7812a38572db84e4e7d6161f8a19f744
Vaarnan Drolia <vaarnan@gmail.com>
2018-08-29 15:18:06 -0700

    Add global subword mode and auto-revert

--- emacs.d/features/editing.el
+++ emacs.d/features/editing.el
@@ -1,3 +1,5 @@
 (use-package expand-region
   :ensure t
   :bind ("C-c w" . er/expand-region))
+
+(global-auto-revert-mode 1)
--- emacs.d/features/navigation.el
+++ emacs.d/features/navigation.el
@@ -1,3 +1,5 @@
 (use-package swiper
   :ensure t
   :bind ("M-s" . swiper))
+
+(global-subword-mode 1)
