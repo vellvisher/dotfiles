0a66232278e8d6ae71506b6b2342a4db97aaa7a2
Vaarnan Drolia <vaarnan@gmail.com>
2018-10-06 16:07:57 +0100

    Goodies for editing like multiple cursors, delete selection mode and drag movement

--- emacs.d/features/editing.el
+++ emacs.d/features/editing.el
@@ -8,4 +8,44 @@
   :ensure t
   :config (global-hungry-delete-mode))
 
+;; Multiple cursors overrides uses this.
+(use-package region-bindings-mode
+  :ensure t
+  :defer 2
+  :config
+  (region-bindings-mode-enable))
 
+(use-package multiple-cursors :ensure t
+  :after region-bindings-mode
+  :init
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  :bind (("C-c a" . mc/mark-all-like-this)
+         ("C-c n" . mc/mark-more-like-this-extended)
+         ("M-1" . mc/mark-next-like-this)
+         ("M-!" . mc/unmark-next-like-this)
+         ("M-2" . mc/mark-previous-like-this)
+         ("M-@" . mc/unmark-previous-like-this)
+         ("M-<mouse-1>" . mc/add-cursor-on-click))
+  :bind (:map region-bindings-mode-map
+              ("a" . mc/mark-all-like-this)
+              ("p" . mc/mark-previous-like-this)
+              ("n" . mc/mark-next-like-this)
+              ("P" . mc/unmark-previous-like-this)
+              ("N" . mc/unmark-next-like-this)
+              ("m" . mc/mark-more-like-this-extended)
+              ("h" . mc-hide-unmatched-lines-mode)
+              ("\\" . mc/vertical-align-with-space)
+              ("#" . mc/insert-numbers) ; use num prefix to set the starting number
+              ("^" . mc/edit-beginnings-of-lines)
+              ("$" . mc/edit-ends-of-lines)))
+
+(use-package drag-stuff
+  :ensure t
+  :bind (("M-p" . drag-stuff-up)
+         ("M-n" . drag-stuff-down)))
+
+(use-package delsel
+  :defer 5
+  :config
+  ;; Override selection with new text.
+  (delete-selection-mode +1))
