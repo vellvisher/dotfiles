4b65fc729134af8d489e56e1e9c3fbedd1ae3d8b
Vaarnan Drolia <vaarnan@gmail.com>
2018-10-11 11:03:55 +0100

    More eshell goodies like fish style auto-suggest and cleanup

--- emacs.d/features/eshell.el
+++ emacs.d/features/eshell.el
@@ -4,9 +4,22 @@
   :config
   (exec-path-from-shell-initialize))
 
-;; Avoid "WARNING: terminal is not fully functional."
-;; http://mbork.pl/2018-06-10_Git_diff_in_Eshell
-(setenv "PAGER" "cat")
-
 (use-package eshell
-  :hook ((eshell-mode . goto-address-mode)))
+  :hook ((eshell-mode . goto-address-mode)
+	 (eshell-mode . v/eshell-mode-hook-function))
+  :init
+  (defun v/eshell-mode-hook-function ()
+    ;; Turn off semantic mode in eshell
+    (semantic-mode -1)
+    (add-to-list 'eshell-visual-commands "ssh")
+
+    ;; Avoid "WARNING: terminal is not fully functional."
+    ;; http://mbork.pl/2018-06-10_Git_diff_in_Eshell
+    (setenv "PAGER" "cat")))
+
+(use-package esh-autosuggest
+  :hook (eshell-mode . esh-autosuggest-mode)
+  ;; If you have use-package-hook-name-suffix set to nil, uncomment and use the
+  ;; line below instead:
+  ;; :hook (eshell-mode-hook . esh-autosuggest-mode)
+  :ensure t)
