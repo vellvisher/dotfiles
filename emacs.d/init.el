f5a84d8d5a9a937e49442ee59ecf015f4eaabbce
Vaarnan Drolia <vaarnan@gmail.com>
2018-10-17 17:39:14 -0400

    Start emacs server so that hg etc can use the same emacs instance

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -103,4 +103,5 @@
 (load "~/.emacs.d/features/editing.el")
 (load "~/.emacs.d/features/git.el")
 (load "~/.emacs.d/features/projectile.el")
+(load "~/.emacs.d/features/server.el")
 ;; (load "~/.emacs.d/features/obc.el")
