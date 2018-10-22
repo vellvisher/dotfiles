287ea81018502ff0aabf6c27a4903dd89a87d835
Vaarnan Drolia <vaarnan@gmail.com>
2018-10-21 20:17:28 -0400

    Move global-auto-revert mode to use-package in files.el

--- emacs.d/features/editing.el
+++ emacs.d/features/editing.el
@@ -2,8 +2,6 @@
   :ensure t
   :bind ("C-c w" . er/expand-region))
 
-(global-auto-revert-mode 1)
-
 (use-package hungry-delete
   :ensure t
   :config (global-hungry-delete-mode))
--- emacs.d/features/files.el
+++ emacs.d/features/files.el
@@ -4,3 +4,14 @@
 (setq auto-save-default nil)
 ;; Avoid creating lock files (ie. .#some-file.el)
 (setq create-lockfiles nil)
+
+(use-package autorevert
+  :config
+  ;; Auto refresh dired.
+  ;; https://mixandgo.com/learn/how-ive-convinced-emacs-to-dance-with-ruby
+  (setq global-auto-revert-non-file-buffers t)
+
+  ;; Be quiet about dired refresh.
+  (setq auto-revert-verbose nil)
+
+  (global-auto-revert-mode))
