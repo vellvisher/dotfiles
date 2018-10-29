d9258fac1e773a699015e87ddd6d05ac1be5cede
Vaarnan Drolia <vaarnan@gmail.com>
2018-10-29 08:19:01 -0500

    Fix eshell imenu by moving to local variable within use-package. Also add eshell history etc.

--- emacs.d/features/eshell.el
+++ emacs.d/features/eshell.el
@@ -13,6 +13,14 @@
     (semantic-mode -1)
     (add-to-list 'eshell-visual-commands "ssh")
 
+    (setq-local imenu-generic-expression '(("Prompt" " $ \\(.*\\)" 1)))
+
+    (setq eshell-history-size (* 10 1024))
+    (setq eshell-hist-ignoredups t)
+    (setq eshell-error-if-no-glob t)
+    (setq eshell-glob-case-insensitive t)
+    (setq eshell-list-files-after-cd nil)
+
     ;; Avoid "WARNING: terminal is not fully functional."
     ;; http://mbork.pl/2018-06-10_Git_diff_in_Eshell
     (setenv "PAGER" "cat")))
@@ -23,5 +31,3 @@
   ;; line below instead:
   ;; :hook (eshell-mode-hook . esh-autosuggest-mode)
   :ensure t)
-
-(setq-local imenu-generic-expression '(("Prompt" " $ \\(.*\\)" 1)))
