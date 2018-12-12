408910b5305e7051764d2409d5a56c4048ac2230
Vaarnan Drolia <vaarnan@gmail.com>
2018-12-12 11:46:30 +0000

    Format ivy.el and add C-g shortcut from xenodium.

--- emacs.d/features/ivy.el
+++ emacs.d/features/ivy.el
@@ -1,26 +1,41 @@
 (use-package counsel
-:ensure t
-:defer 0.1
-:config
-;; Smex handles M-x command sorting. Bringing recent commands to the top.
-(use-package smex
-:ensure t)
-;; Wgrep is used by counsel-ag (to make writeable).
-(use-package wgrep
-:ensure t)
-(counsel-mode +1))
+  :ensure t
+  :defer 0.1
+  :config
+  ;; Smex handles M-x command sorting. Bringing recent commands to the top.
+  (use-package smex
+    :ensure t)
+  ;; Wgrep is used by counsel-ag (to make writeable).
+  (use-package wgrep
+    :ensure t)
+  (counsel-mode +1))
 
 (use-package ivy
-:ensure t
-:defer 0.1
-:bind ("C-x C-b" . ivy-switch-buffer)
-:config
-(setq ivy-height 40)
-(setq ivy-count-format "")
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(ivy-mode +1))
-
-(use-package counsel-projectile
   :ensure t
+  :defer 0.1
+  :bind (("C-x C-b" . ivy-switch-buffer)
+	 :map ivy-minibuffer-map
+	 ("C-g" . v/ivy-keyboard-quit-dwim))
+  :config
+  (setq ivy-height 40)
+  (setq ivy-count-format "")
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (defun v/ivy-keyboard-quit-dwim () 
+    "If region active, deactivate. If there's content, minibuffer. Otherwise quit." 
+    (interactive) 
+    (cond ((and 
+	    delete-selection-mode
+	    (region-active-p)) 
+	   (setq deactivate-mark t)) 
+	  ((> (length ivy-text) 0) 
+	   (delete-minibuffer-contents)) 
+	  (t (minibuffer-keyboard-quit))))
+  (ivy-mode +1))
+
+(use-package counsel-projectile 
+  :ensure t 
   :bind ("C-x f" . counsel-projectile-find-file))
+
+(use-package ivy-hydra 
+  :ensure t)
