53db7a578e241e22d327b39b86ee3bdf143238e7
Vaarnan Drolia <vaarnan@gmail.com>
2018-12-30 17:50:08 +0800

    Make org follow links on RET and change org-default-notes-file to beorg/inbox.org

--- emacs.d/features/org.el
+++ emacs.d/features/org.el
@@ -1,3 +1,10 @@
 (require 'v-vsetq)
 
-(v/vsetq org-default-notes-file "~/beorg/notes.org")
+
+(use-package org-web-tools
+  :ensure t)
+
+(use-package org
+  :config
+  (v/vsetq org-default-notes-file "~/beorg/inbox.org")
+  (v/vsetq org-return-follows-link t))
