d35301f736ca4e6fce8fbd9229c9ee5667f0ba9d
Vaarnan Drolia <vaarnan@gmail.com>
2018-12-30 18:17:53 +0800

    Change default org-directory to beorg and fix org-default-notes-file

--- emacs.d/features/org.el
+++ emacs.d/features/org.el
@@ -1,10 +1,10 @@
 (require 'v-vsetq)
 
-
 (use-package org-web-tools
   :ensure t)
 
 (use-package org
   :config
-  (v/vsetq org-default-notes-file "~/beorg/inbox.org")
+  (v/vsetq org-directory "~/beorg")
+  (v/vsetq org-default-notes-file (concat org-directory "/inbox.org"))
   (v/vsetq org-return-follows-link t))
