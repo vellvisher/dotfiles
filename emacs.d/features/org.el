6f3dcfb975600ff2f30d412074f01ef30cee5123
Vaarnan Drolia <vaarnan@gmail.com>
2019-01-06 09:43:53 -0500

    [org] Make org agenda files to beorg directory

--- emacs.d/features/org.el
+++ emacs.d/features/org.el
@@ -7,4 +7,5 @@
   :config
   (v/vsetq org-directory "~/beorg")
   (v/vsetq org-default-notes-file (concat org-directory "/inbox.org"))
+  (v/vsetq org-agenda-files (make-list 1 org-directory))
   (v/vsetq org-return-follows-link t))
