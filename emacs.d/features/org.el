60e6b132f2e857829b32cb71e466128c420eddf3
Vaarnan Drolia <vaarnan@gmail.com>
2019-01-07 18:38:05 -0500

    [org] Add capture template for TODO and shortcuts to add org links

--- emacs.d/features/org.el
+++ emacs.d/features/org.el
@@ -4,8 +4,17 @@
   :ensure t)
 
 (use-package org
+  :bind (("C-c c" . org-capture)
+	 ("C-c a" . org-agenda-list)
+	 ("C-c l" . org-store-link)
+	 :map org-mode-map
+	 ("C-c C-l" . org-insert-link))
   :config
   (v/vsetq org-directory "~/beorg")
   (v/vsetq org-default-notes-file (concat org-directory "/inbox.org"))
   (v/vsetq org-agenda-files (make-list 1 org-directory))
-  (v/vsetq org-return-follows-link t))
+  (v/vsetq org-return-follows-link t)
+  
+  (setq org-capture-templates
+      '(("t" "TODO" entry (file "")
+         "* TODO %?\nSCHEDULED:%^t"))))
