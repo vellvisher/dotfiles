16546a809815f8c6aec6063d5c884954d79a08e1
Vaarnan Drolia <vaarnan@gmail.com>
2019-02-09 15:38:46 -0600

    [org] Goodies

--- emacs.d/features/org.el
+++ emacs.d/features/org.el
@@ -9,11 +9,19 @@
 	 ("C-c l" . org-store-link)
 	 :map org-mode-map
 	 ("C-c C-l" . org-insert-link))
+  :hook((org-mode . v/org-mode-hook-function))
+  :init
+  (defun v/org-mode-hook-function ()
+    (toggle-truncate-lines 0)
+    (org-display-inline-images)
+    (v/vsetq show-trailing-whitespace t)
+    (set-fill-column 1000))
   :config
   (v/vsetq org-directory "~/beorg")
   (v/vsetq org-default-notes-file (concat org-directory "/inbox.org"))
   (v/vsetq org-agenda-files (make-list 1 org-directory))
   (v/vsetq org-return-follows-link t)
+  (v/vsetq org-catch-invisible-edits 'error)
   
   (setq org-capture-templates
       '(("t" "TODO" entry (file "")
