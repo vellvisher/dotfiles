5a309d3632e5a6eabddf69e61bd9127e29e240d9
Vaarnan Drolia <vaarnan@gmail.com>
2019-02-09 15:21:27 -0600

    [ui] Column num and time in modeline

--- emacs.d/features/ui.el
+++ emacs.d/features/ui.el
@@ -4,3 +4,9 @@
 
 ;; Always use y/n instead of yes/no prompts.
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Display time in modeline.
+(display-time-mode 1)
+
+;; Display column numbers.
+(setq-default column-number-mode t)
