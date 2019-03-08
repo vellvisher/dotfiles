a106af81871b38fe24bd7db6fe0c948c6783c06d
Vaarnan Drolia <vaarnan@gmail.com>
2019-03-08 12:49:42 +0000

    [editing] Move inside use-package to not have them loaded/parsed until binding invoked

--- emacs.d/features/editing.el
+++ emacs.d/features/editing.el
@@ -54,23 +54,23 @@
 (use-package drag-stuff
   :ensure t
   :bind (("M-p" . v/drag-stuff-up)
-         ("M-n" . v/drag-stuff-down)))
+         ("M-n" . v/drag-stuff-down))
+  :config
+  ;; org-metaup/down in org-mode
+  (defun v/drag-stuff-down (arg)
+    (interactive "p")
+    "Drag stuff ARG lines down."
+    (if (eq major-mode 'org-mode)
+        (org-metadown)
+      (drag-stuff-down arg)))
 
-;; org-metaup/down in org-mode
-(defun v/drag-stuff-down (arg)
-  (interactive "p")
-  "Drag stuff ARG lines down."
-  (if (eq major-mode 'org-mode)
-      (org-metadown)
-    (drag-stuff-down arg)))
-
-;; org-metaup/down in org-mode
-(defun v/drag-stuff-up (arg)
-  (interactive "p")
-  "Drag stuff ARG lines up."
-  (if (eq major-mode 'org-mode)
-      (org-metaup)
-    (drag-stuff-up arg)))
+  ;; org-metaup/down in org-mode
+  (defun v/drag-stuff-up (arg)
+    (interactive "p")
+    "Drag stuff ARG lines up."
+    (if (eq major-mode 'org-mode)
+        (org-metaup)
+      (drag-stuff-up arg))))
 
 (use-package delsel
   :defer 5
