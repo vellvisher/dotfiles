abf846b03e175f8da2ec65326464cddad2bb9ce3
Vaarnan Drolia <vaarnan@gmail.com>
2019-04-29 10:14:38 +0530

    [ui] Add battery to modeline for laptops

--- emacs.d/features/ui.el
+++ emacs.d/features/ui.el
@@ -8,5 +8,14 @@
 ;; Display time in modeline.
 (display-time-mode 1)
 
+;; Display battery level in modeline.
+(use-package battery
+  :config
+  (when (and battery-status-function
+       (not (string-match-p "N/A"
+                (battery-format "%B"
+                        (funcall battery-status-function)))))
+  (display-battery-mode 1)))
+
 ;; Display column numbers.
 (setq-default column-number-mode t)
