94c75ae0d8361d3504c79f3fb66597b022a041e8
Vaarnan Drolia <vaarnan@gmail.com>
2019-06-10 14:52:59 +0100

    [bazel] Add support to add target from file after ":" with v/bazel-insert-completed-target

--- emacs.d/features/fe-bazel.el
+++ emacs.d/features/fe-bazel.el
@@ -3,6 +3,22 @@
   :mode (("\\.bzl\\'" . bazel-mode)
          ("BUILD\\'" . bazel-mode))
   :hook (bazel-mode . v/bazel-mode-hook-function)
+  :bind (:map bazel-mode-map
+         (":" . v/bazel-insert-completed-target))
+  :config
+  (defun v/bazel-insert-completed-target ()
+    (interactive)
+    (self-insert-command 1)
+    (if (looking-back "\":")
+        (insert (completing-read
+                 "Build rule: "
+                 (save-excursion
+                   (goto-char (point-min))
+                   (let ((results '()))
+                     (while (re-search-forward "name *= *\"\\(.*\\)\"" nil t)
+                       (add-to-list 'results (match-string 1)))
+                     (when (> (length results) 0)
+                       results)))))))
   :init
   (defun v/bazel-mode-hook-function ()
     ;; Format files for me.
