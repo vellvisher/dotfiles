ce5fb2ef7ac0a46346b4f81e43993bb2407d9b0c
Vaarnan Drolia <vaarnan@gmail.com>
2019-11-27 12:19:08 +0000

    [prog] Add hook for prettify-symbols-mode

--- emacs.d/features/fe-prog.el
+++ emacs.d/features/fe-prog.el
@@ -1,18 +1,25 @@
+(require 'v-vsetq)
+
 (use-package prog-mode
   ;; :bind (:map
   ;;        prog-mode-map
   ;;        ([f6] . recompile))
   :hook ((prog-mode . flyspell-prog-mode)
-	 (prog-mode . goto-address-prog-mode)
-         (prog-mode . rainbow-mode))
-         ;; (prog-mode . flycheck-mode)
-	 ;; (prog-mode . company-mode)
-         ;; (prog-mode . yas-minor-mode)
-         ;; (prog-mode . centered-cursor-mode)
+         (prog-mode . goto-address-prog-mode)
+         (prog-mode . prettify-symbols-mode)
+         (prog-mode . rainbow-mode)
+         (prog-mode . v/prog-mode-hook-function))
+  ;; (prog-mode . flycheck-mode)
+  ;; (prog-mode . company-mode)
+  ;; (prog-mode . yas-minor-mode)
+  ;; (prog-mode . centered-cursor-mode)
   :config
   (require 'flyspell)
   ;; (require 'flycheck)
 
   ;; Highlight hex strings in respective color.
   (use-package rainbow-mode
-    :ensure t))
+    :ensure t)
+  (defun v/prog-mode-hook-function ()
+    (v/vsetq prettify-symbols-alist '(("lambda" . 955)
+                                      ("->" . 8594)))))
