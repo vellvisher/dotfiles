c3e0a030ce175e7f467a898cba34a7fb5f6e721d
Vaarnan Drolia <vaarnan@gmail.com>
2020-02-08 12:47:54 +0000

    [ediff] Disable golden-ratio-mode for ediff since it makes the meta mode too big

--- emacs.d/features/fe-ediff.el
+++ emacs.d/features/fe-ediff.el
@@ -4,5 +4,11 @@
   ;; From https://oremacs.com/2015/01/17/setting-up-ediff/
   (v/csetq ediff-window-setup-function 'ediff-setup-windows-plain)
   (v/csetq ediff-split-window-function 'split-window-horizontally)
+
+  ;; winner-mode
   (winner-mode)
-  (add-hook 'ediff-after-quit-hook-internal 'winner-undo))
+  (add-hook 'ediff-after-quit-hook-internal 'winner-undo)
+
+  ;; golden-ratio disable only for ediff mode
+  (add-hook 'ediff-mode-hook (lambda () (golden-ratio-mode -1)))
+  (add-hook 'ediff-after-quit-hook-internal 'golden-ratio-mode))
