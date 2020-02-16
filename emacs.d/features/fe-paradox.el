5c043c062f886d8d088d371ded9ee5b762e8e361
Vaarnan Drolia <vaarnan@gmail.com>
2020-02-16 18:13:37 +0000

    [paradox] Add filter when "/" is pressed.

--- emacs.d/features/fe-paradox.el
+++ emacs.d/features/fe-paradox.el
@@ -2,5 +2,8 @@
 (use-package paradox
   :ensure t
   :commands (paradox-list-packages)
+  :bind (:map
+         paradox-menu-mode-map
+         ("/" . paradox-filter-regexp))
   :config
   (fullframe paradox-list-packages paradox-quit-and-close))
