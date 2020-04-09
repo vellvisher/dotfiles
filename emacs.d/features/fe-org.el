99702f32353da5727fa5da3d4913ab7dd50bed80
Vaarnan Drolia <vaarnan@gmail.com>
2020-04-09 05:06:15 +0100

    [babel] Add sqlite for ob-babel and defer other packages.

--- emacs.d/features/fe-org.el
+++ emacs.d/features/fe-org.el
@@ -146,15 +146,19 @@ Booking reference: %s
   :config
   ;; TODO: Add lazy instantiation.
   (use-package ob-swift
-    :ensure t)
+    :ensure t
+    :defer 10)
   ;; TODO: Add lazy instantiation.
   (use-package ob-kotlin
-    :ensure t)
+    :ensure t
+    :defer 10)
   (use-package ob-async
-    :ensure t)
+    :ensure t
+    :defer 10)
   (org-babel-do-load-languages
    'org-babel-load-languages '((shell . t)
-                               (emacs-lisp . t))))
+                               (emacs-lisp . t)
+                               (sqlite . t))))
 
 ;; TODO: Add lazy instantiation.
 ;; https://stackoverflow.com/questions/41517257/execute-java-code-block-in-org-mode
