17ccfc865ee17480000ee75716c39cefb350b4f6
Vaarnan Drolia <vaarnan@gmail.com>
2021-12-02 15:38:23 +0000

    [yas] Disable yasnippet-snippets for the moment and enable the minor mode for email compose

--- emacs.d/features/fe-yasnippet.el
+++ emacs.d/features/fe-yasnippet.el
@@ -4,20 +4,17 @@
   ;; :validate-custom
   ;; (yas-indent-line 'fixed)
   ;; (yas-triggers-in-field t)
-  :commands (yas-expand
-             yas-minor-mode)
+  ;; :commands (yas-expand yas-minor-mode)
   :hook ((prog-mode . yas-minor-mode)
-         (org-mode . yas-minor-mode))
+         (org-mode . yas-minor-mode)
+         (mu4e-compose-mode . yas-minor-mode))
   :config
   ;; (use-package ar-yas
   ;;   :commands (v/yas-install--github-yasnippets
   ;;              v/yas-install-external-yasnippets))
 
-  (use-package yasnippet-snippets
-    :ensure t)
-
-  (add-to-list 'yas-snippet-dirs
-               "~/.emacs.d/snippets/" t)
+  ;; (use-package yasnippet-snippets
+  ;;   :ensure t)
 
   (yas-reload-all))
 
