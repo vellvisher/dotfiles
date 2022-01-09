66487b84d172d79083c99e1d85a3fd1bd0bd8aa9
Vaarnan Drolia <vaarnan@gmail.com>
2022-01-09 16:26:07 +0000

    [projectile] Assume or install fd

--- emacs.d/features/fe-projectile.el
+++ emacs.d/features/fe-projectile.el
@@ -2,7 +2,7 @@
   :ensure t)
 
 (use-package projectile
-  :ensure t
+  :ensure-system-package fd
   :defer 2
   :bind (("C-c s" . counsel-projectile-rg))
   :config
@@ -12,13 +12,13 @@
 
   (setq projectile-ignored-projects '("~/"))
   ;; fd is super fast. Use it if available.
-  (when (executable-find "fd")
-    (let ((fd-command "fd . --print0"))
-      (setq projectile-hg-command fd-command)
-      (setq projectile-git-command fd-command)
-      (setq projectile-fossil-command fd-command)
-      (setq projectile-bzr-command fd-command)
-      (setq projectile-darcs-command fd-command)
-      (setq projectile-svn-command fd-command)
-      (setq projectile-generic-command fd-command)))
+
+  (let ((fd-command "fd . --print0"))
+    (setq projectile-hg-command fd-command)
+    (setq projectile-git-command fd-command)
+    (setq projectile-fossil-command fd-command)
+    (setq projectile-bzr-command fd-command)
+    (setq projectile-darcs-command fd-command)
+    (setq projectile-svn-command fd-command)
+    (setq projectile-generic-command fd-command))
   (projectile-mode))
