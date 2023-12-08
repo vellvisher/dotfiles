b0e385ecc1082d447d56896e4280ca140764d32f
Vaarnan Drolia <vaarnan@gmail.com>
2023-12-08 08:36:27 -0800

    [llm] Add dall-e version picker

--- emacs.d/features/fe-llm.el
+++ emacs.d/features/fe-llm.el
@@ -11,8 +11,20 @@
 
 (use-package dall-e-shell
   :ensure t
+  :bind (:map dall-e-shell-mode-map ("C-c C-v" . v/dall-e-shell-swap-model-version))
   :custom
   ((dall-e-shell-openai-key
     (lambda ()
       (auth-source-pick-first-password :host "api.openai.com")))
-   (dall-e-shell-image-output-directory	"~/tmp_dall_e")))
+   (dall-e-shell-image-output-directory	"~/tmp_dall_e"))
+  :config
+  ;; From https://platform.openai.com/docs/models/dall-e
+  (v/csetq dall-e-shell-model-versions '("dall-e-3" "dall-e-2"))
+  (defun v/dall-e-shell-swap-model-version ()
+  "Swap model version from `dall-e-shell-model-versions'."
+  (interactive)
+  (unless (eq major-mode 'dall-e-shell-mode)
+    (user-error "Not in a shell"))
+  (setq dall-e-shell-model-version
+              (completing-read "Model version: "
+                                 dall-e-shell-model-versions nil t))))
