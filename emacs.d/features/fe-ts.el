6ed13b380dbe7673ee99e8ab1b7a04fe31dc4a1f
Vaarnan Drolia <vaarnan@gmail.com>
2024-03-28 12:06:50 +0000

    [ts] Add prettier and fix bug with specifying as dart

--- emacs.d/features/fe-ts.el
+++ emacs.d/features/fe-ts.el
@@ -1,7 +1,13 @@
 (require 'v-vsetq)
 
 (use-package typescript-mode
-  :mode ("\\.ts\\'" . dart-mode)
+  :mode ("\\.ts\\'" . typescript-mode)
   :ensure t
   :config
-  (v/vsetq typescript-indent-level 2))
+  (v/vsetq typescript-indent-level 2)
+  (add-hook 'typescript-mode-hook
+            (lambda () (add-hook 'before-save-hook 'prettier-js nil t))))
+
+(use-package prettier-js
+  :after typescript-mode
+  :ensure t)
