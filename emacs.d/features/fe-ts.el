c92d6d4e30a91a2e5d9a78aa6da175dcf15cfa07
Vaarnan Drolia <vaarnan@gmail.com>
2024-04-14 10:20:29 +0100

    [ts] Highlight at 100 col for typescript

--- emacs.d/features/fe-ts.el
+++ emacs.d/features/fe-ts.el
@@ -6,7 +6,9 @@
   :config
   (v/vsetq typescript-indent-level 2)
   (add-hook 'typescript-mode-hook
-            (lambda () (add-hook 'before-save-hook 'prettier-js nil t))))
+            (lambda ()
+              (add-hook 'before-save-hook 'prettier-js nil t)
+              (setq-local whitespace-line-column 100))))
 
 (use-package prettier-js
   :after typescript-mode
