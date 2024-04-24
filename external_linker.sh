213d0c2c24d9ed6eebe9949e20d4d5e72fb0bff6
Vaarnan Drolia <vaarnan@gmail.com>
2024-04-24 07:39:19 +0100

    Updates to external_linker

--- external_linker.sh
+++ external_linker.sh
@@ -6,6 +6,8 @@ echo "Press <TMUX-LEADER>+I to install tpm plugins"
 brew install ctags global
 pip install pygments
 
+# + [ ] Native compile needs gcc/libgccjit, https://github.com/d12frosted/homebrew-emacs-plus/issues/309
+brew install rg fd aspell swift-format trash mu isync webp ffmpeg gcc libgccjit
 
 # New stuff!
 # --with-mailutils probably not necessary
@@ -13,4 +15,3 @@ brew install emacs-plus@29 --with-no-frame-refocus --with-modern-sexy-v1-icon --
 # + [ ] Install emacs font in fe-ui.el
 # + [ ] link google drive
 # + [ ] ln -s $HOME/Library/CloudStorage/GoogleDrive-vaarnan@gmail.com/My\ Drive $HOME/GoogleDriveGmail
-brew install rg fd aspell swift-format trash mu isync
