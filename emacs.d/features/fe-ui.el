73b5233b83f8d4e76e019ac5ee9c40acec2e2f1d
Vaarnan Drolia <vaarnan@gmail.com>
2025-11-02 17:35:12 +0000

    Cask does not really work for the font but the command still works

--- emacs.d/features/fe-ui.el
+++ emacs.d/features/fe-ui.el
@@ -102,7 +102,7 @@
 (set-face-attribute 'default nil :stipple nil :foreground "#eeffff" :inverse-video nil
                     ;; :family "Menlo" ;; or Meslo if unavailable: https://github.com/andreberg/Meslo-Font
                     ;; :family "Hack" ;; brew tap homebrew/cask-fonts && brew cask install font-hack
-                    :family "JetBrains Mono" ;; brew tap homebrew/cask-fonts && brew install --cask font-jetbrains-mono
+                    :family "JetBrains Mono" ;; brew install --cask font-jetbrains-mono
                     ;; :family "mononoki" ;; https://madmalik.github.io/mononoki/ or sudo apt-get install fonts-mononoki
                     :box nil :strike-through nil :overline nil :underline nil
                     :slant 'normal :weight 'normal :width 'normal :foundry "nil"
