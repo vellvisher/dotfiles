e5de41250c11e9f648a62c18631d14023ac9dbf9
Vaarnan Drolia <vaarnan@gmail.com>
2025-12-01 17:25:51 +0000

    Add macos share to emacs

--- emacs.d/features/fe-mac.el
+++ emacs.d/features/fe-mac.el
@@ -6,3 +6,8 @@
   "Use \"trash\" to move FILE to the system trash."
   (cl-assert (executable-find "trash") nil "'trash' must be installed. Needs \"brew install trash\"")
   (call-process (executable-find "trash") nil 0 nil file))
+
+(use-package macos
+  :vc (:url "https://github.com/xenodium/EmacsMacOSModule")
+  :config
+  (macos-load-module))
